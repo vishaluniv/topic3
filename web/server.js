49ad12eb60857f2bc75aa547f852ae26a8a2010b
vishaluniv <vishal4855.be21@chitkara.edu.in>
2023-02-11 10:23:43 +0530

    Updated server.js

--- web/server.js
+++ web/server.js
@@ -6,13 +6,20 @@ const base = `${__dirname}/public`;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/', function (req, res) {
+    res.sendFile(`${base}/device-list.html`);
   });
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(`${base}/404.html`);
+  });
+
+
 
 
 
